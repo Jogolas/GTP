2846a240969051dbd8ad222e9006a7c2ead9c5d9
Stephen Doonan <stephenjkd1993@hotmail.co.uk>
2018-03-28 05:30:56 +0100

    Phong Shader set up (no texture) Learning a little about shaders before I start going ham on them.  I know we are getting close to the deadline but this will pay off.

--- 404GameUnnamed/Scene.cpp
+++ 404GameUnnamed/Scene.cpp
@@ -25,7 +25,7 @@ Scene::Scene(bool active)
 	crates[6] = new Environment(glm::vec3(-36, 2, 0), glm::vec3(12, 3, 12), 0, glm::vec3(0, 1, 0));
 	crates[7] = new Environment(glm::vec3(36, 2, 0), glm::vec3(12, 3, 12), 0, glm::vec3(0, 1, 0));
 
-	lightingShader = Shader("simpleShader.vert", "simpleShader.frag");
+	lightingShader = Shader("phongShader.vert", "phongShader.frag");
 	lampShader = Shader("simpleShader.vert", "simpleShader.frag");
 
 	bossObject = Model("models/BossModel.obj");
@@ -83,6 +83,8 @@ void Scene::drawScene()
 	lightingShader.use();
 	lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
 	lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+	lightingShader.setVec3("lightPos", lightPos);
+	lightingShader.setVec3("viewPos", player->cam.Position);
 
 	// view and projection transformations
 	glm::mat4 projection = glm::perspective(glm::radians(player->cam.Zoom), 800.0f / 600.0f, 1.0f, 500.0f);
@@ -95,10 +97,13 @@ void Scene::drawScene()
 	glm::mat4 model = player->draw();
 
 	lightingShader.setMat4("model", model);
-	bossObject.Draw(lightingShader);
+	bossObject.DrawMesh(lightingShader);
 
 
 	//ground
+
+	lightingShader.setVec3("objectColor", 0.5f, 0.5f, 0.5f);
+
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	model = glm::mat4(1.0); // reset model matrix
@@ -109,12 +114,16 @@ void Scene::drawScene()
 
 
 	//walls
+	lightingShader.setVec3("objectColor", 1.0f, 0.5f, 1.0f);
+
 	for (int i = 0; i < 4; i++) {
 		model = glm::mat4(1.0); // reset model matrix
 		model = wall[i]->draw();
 		lightingShader.setMat4("model", model);
 		cubeObject.DrawMesh(lightingShader);
 	}
+
+	lightingShader.setVec3("objectColor", 0.5f, 1.0f, 1.0f);
 
 	for (int i = 0; i < 8; i++) {
 		model = glm::mat4(1.0); // reset model matrix
@@ -125,176 +134,6 @@ void Scene::drawScene()
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
-
-//void drawScenes()
-//{
-//	glm::mat4 projection(1.0);
-//	projection = glm::perspective(float(60.0f * DEG_TO_RADIAN), 800.0f / 600.0f, 1.0f, 300.0f);
-//
-//
-//	glm::mat4 modelview(1.0);
-//
-//	mvStack.push(modelview);
-//	mvStack.top() = cam->draw(mvStack.top(), dynamic_cast<Player*>(player));
-//
-//	// skybox as single cube using cube map
-//	glCullFace(GL_FRONT);
-//	glUseProgram(skyProgram);
-//	Renderer::setMatrix(skyProgram, "projection", glm::value_ptr(projection));
-//	glDepthMask(GL_FALSE); // make sure writing to update depth test is off
-//	glm::mat3 mvRotOnlyMat3 = glm::mat3(mvStack.top());
-//	mvStack.push(glm::mat4(mvRotOnlyMat3));
-//
-//	glBindTexture(GL_TEXTURE_CUBE_MAP, skybox[0]);
-//	mvStack.top() = glm::scale(mvStack.top(), glm::vec3(1.5f, 1.5f, 1.5f));
-//	Renderer::setMatrix(skyProgram, "modelview", glm::value_ptr(mvStack.top()));
-//	meshes[0].drawMesh(meshes[1].getMeshID());
-//	mvStack.pop();
-//	glCullFace(GL_BACK);
-//
-//
-//	// back to remainder of rendering
-//	glDepthMask(GL_TRUE); // make sure depth test is on
-//
-//	glm::vec4 tmp = mvStack.top() * lightPos;
-//	light.position[0] = tmp.x;
-//	light.position[1] = tmp.y;
-//	light.position[2] = tmp.z;
-//
-//
-//
-//	shader->bindShaderProgram(program[0]);
-//	shader->useMatrix4fv(projection, "projection");
-//	Renderer::setLightPos(program[0], glm::value_ptr(tmp));
-//
-//	////ground
-//	glBindTexture(GL_TEXTURE_2D, texture[1]);
-//	glm::mat4 modelMatrix(1.0);
-//	mvStack.push(mvStack.top());
-//	modelMatrix = ground->draw(modelMatrix);
-//	mvStack.top() *= modelMatrix;
-//	shader->useMatrix4fv(mvStack.top(), "modelview");
-//	ground->getDrawingObject()->getMesh().drawMesh(ground->getDrawingObject()->getMesh().getMeshID());
-//	mvStack.pop();
-//
-//	//// walls
-//	glBindTexture(GL_TEXTURE_2D, texture[2]);
-//	for (GLuint i = 0; i < 4; i++) {
-//
-//		modelMatrix = glm::mat4(1.0); //reset modelmatrix
-//		mvStack.push(mvStack.top());
-//		modelMatrix = wall[i]->draw(modelMatrix);
-//		mvStack.top() *= modelMatrix;
-//		shader->useMatrix4fv(mvStack.top(), "modelview");
-//		wall[i]->getDrawingObject()->getMesh().drawMesh(wall[i]->getDrawingObject()->getMesh().getMeshID());
-//		mvStack.pop();
-//	}
-//
-//	//// crates
-//	glBindTexture(GL_TEXTURE_2D, texture[2]);
-//	for (GLuint i = 0; i < 8; i++) {
-//
-//		modelMatrix = glm::mat4(1.0); //reset modelmatrix
-//		mvStack.push(mvStack.top());
-//		modelMatrix = crates[i]->draw(modelMatrix);
-//		mvStack.top() *= modelMatrix;
-//		shader->useMatrix4fv(mvStack.top(), "modelview");
-//		crates[i]->getDrawingObject()->getMesh().drawMesh(crates[i]->getDrawingObject()->getMesh().getMeshID());
-//		mvStack.pop();
-//	}
-//
-//	shader->unbindShaderProgram();
-//
-//
-//	shader->bindShaderProgram(program[1]);
-//	shader->useMatrix4fv(projection, "projection");
-//	Renderer::setLightPos(program[1], glm::value_ptr(tmp));
-//	glActiveTexture(GL_TEXTURE1);
-//	glBindTexture(GL_TEXTURE_CUBE_MAP, skybox[0]);
-//	glActiveTexture(GL_TEXTURE0);
-//	glBindTexture(GL_TEXTURE_2D, texture[0]);
-//
-//	////player
-//	mvStack.push(mvStack.top());
-//	mvStack.top() = glm::translate(mvStack.top(), glm::vec3(0, 0, 0));
-//	mvStack.top() = glm::scale(mvStack.top(), glm::vec3(2, 2, 2));
-//	shader->useMatrix4fv(mvStack.top(), "modelview");
-//	fbxMesh.Draw(program[1]);
-//	
-//
-//	//mvStack.push(mvStack.top());
-//	//modelMatrix = glm::mat4(1.0); //reset model matrix
-//	//modelMatrix = player->draw(modelMatrix, glm::vec3(1, 1, 1));
-//	//fbxMesh.Draw(program[1]);
-//	//mvStack.top() *= modelMatrix;
-//	//shader->useMatrix4fv(mvStack.top(), "modelview");
-//	//shader->useMatrix4fv(modelMatrix, "modelMatrix");
-//	//GLuint uniformIndex = glGetUniformLocation(program[1], "cameraPos");
-//	//glUniform3fv(uniformIndex, 1, glm::value_ptr(dynamic_cast<Player*>(player)->getEye()));
-//	//Renderer::setMaterial(program[1], material);
-//	////player->getDrawingObject()->getMesh().drawMesh(player->getDrawingObject()->getMesh().getMeshID());
-//	//player->getDrawingObject()->getFBXMesh().Draw(program[1]);
-//	//player->getDrawingObject()->getMesh().drawFBXMesh(program[1]);
-//	//mvStack.pop();
-//
-//	////UI elements
-//	glDisable(GL_DEPTH_TEST); // make sure writing to update depth test is off
-//	glBindTexture(GL_TEXTURE_2D, UITexture[0]);
-//	modelMatrix = glm::mat4(1.0); //reset modelmatrix
-//	mvStack.push(mvStack.top());
-//	modelMatrix = UI[0]->draw(modelMatrix);
-//	mvStack.top() *= modelMatrix;
-//	glm::value_ptr(glm::mat4(1.0));
-//	shader->useMatrix4fv(mvStack.top(), "modelview");
-//	GLuint uniformIndex = glGetUniformLocation(program[1], "cameraPos");
-//	UI[0]->getDrawingObject()->getMesh().drawMesh(UI[0]->getDrawingObject()->getMesh().getMeshID());
-//	mvStack.pop();
-//	glEnable(GL_DEPTH_TEST);
-//
-//	glBindTexture(GL_TEXTURE_2D, texture[1]);
-//	////boss
-//	mvStack.push(mvStack.top());
-//	modelMatrix = glm::mat4(1.0); //reset model matrix
-//	modelMatrix = dynamic_cast<NPC*>(boss)->draw(modelMatrix);
-//	mvStack.top() *= modelMatrix;
-//	shader->useMatrix4fv(mvStack.top(), "modelview");
-//	shader->useMatrix4fv(modelMatrix, "modelMatrix");
-//	uniformIndex = glGetUniformLocation(program[1], "cameraPos");
-//	glUniform3fv(uniformIndex, 1, glm::value_ptr(dynamic_cast<Player*>(player)->getEye()));
-//	Renderer::setMaterial(program[1], material);
-//	dynamic_cast<NPC*>(boss)->getDrawingObject()->getMesh().drawMesh(dynamic_cast<NPC*>(boss)->getDrawingObject()->getMesh().getMeshID());
-//	mvStack.pop();
-//	shader->unbindShaderProgram();
-//
-//	shader->bindShaderProgram(program[0]);
-//	shader->useMatrix4fv(projection, "projection");
-//
-//	if (dynamic_cast<NPC*>(boss)->getSpell() != nullptr) {
-//		glBindTexture(GL_TEXTURE_2D, texture[3]);
-//		mvStack.push(mvStack.top());
-//		mvStack.top() = glm::translate(mvStack.top(), dynamic_cast<NPC*>(boss)->getSpell()->getPosition());
-//		mvStack.top() = glm::scale(mvStack.top(), glm::vec3(0.25f, 0.25f, 0.25f));
-//		Renderer::setMatrix(program[0], "modelview", glm::value_ptr(mvStack.top()));
-//		Renderer::drawObj(meshes[1].getMeshID(), meshes[1].getIndexCount(), GL_TRIANGLES);
-//		mvStack.pop();
-//	}
-//
-//
-//	////node locations
-//	for (int i = 0; i < 4; i++) {
-//		mvStack.push(mvStack.top());
-//		mvStack.top() = glm::translate(mvStack.top(), graph.GetNode(i)->debug_position);
-//		mvStack.top() = glm::scale(mvStack.top(), glm::vec3(0.25f, 0.25f, 0.25f));
-//		Renderer::setMatrix(program[0], "modelview", glm::value_ptr(mvStack.top()));
-//		Renderer::drawObj(meshes[1].getMeshID(), meshes[1].getIndexCount(), GL_TRIANGLES);
-//		mvStack.pop();
-//	}
-//
-//
-//	shader->unbindShaderProgram();
-//
-//	mvStack.pop(); //initial matrix
-//}
 
 
 void Scene::collisions()
@@ -316,6 +155,4 @@ void Scene::updateScene()
 	player->update();
 	collisions();
 	mouse.MouseMotion(player);
-
-
 }
--- 404GameUnnamed/phongShader.frag
+++ 404GameUnnamed/phongShader.frag
@@ -4,8 +4,41 @@ out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+in vec3 Normal;
+in vec3 FragPos;
 
 void main()
 {
-	FragColor = vec4(lightColor * objectColor, 1.0);
+	// Setting up Ambient Light
+	float ambStr = 0.1;
+	vec3 ambient = ambStr * lightColor;
+	
+	//"When calculating lighting we usually do not care about the magnitude of a vector or their position; we only care about their direction. 
+	//Because we only care about their direction almost all the calculations are done with unit vectors since it simplifies most 
+	//calculations (like the dot product). 
+	//So when doing lighting calculations, make sure you always normalize the relevant vectors to ensure they're actual unit vectors. 
+	//Forgetting to normalize a vector is a popular mistake." - JoeyDeVries, https://learnopengl.com/Lighting/Basic-Lighting
+
+	// Setting up Diffuse Light
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColor;
+
+	// Setting up Specular Light
+	float specStr = 0.5;
+
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+	vec3 specular = specStr * spec * lightColor;
+
+
+	vec3 result = (ambient + diffuse + specular) * objectColor;
+	FragColor = vec4(result, 1.0);
 }
